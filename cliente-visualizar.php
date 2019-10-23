97d4b03f08b0aa6b56f3d79befbec7a481e80f8e
Programador Web 2019.2 <16124512019.2@senacrjedu.local>
2019-10-23 13:19:14 -0300

    atualizando cliente

--- cliente-visualizar.php
+++ cliente-visualizar.php
@@ -1,77 +1,121 @@
 <?php
-  include "includes/conexao.php";
+  if($_POST){
+    // INCLUIR O ARQUIVO DE CONEXÃO
+    include "includes/conexao.php";
+    include "includes/funcoes.php";
+    // CAPTURAR OS DADOS DO POST
+    $nome = $_POST['nome'];
+    $telefone = $_POST['telefone'];
+    $email = $_POST['email'];
+    $cpf = $_POST['cpf'];
+    $id = $_POST['id'];
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+      $msg = "E-mail inválido";
+    
+    }else if(!validaCPF($cpf)){  
+      $msg = "CPF inválido";
+    }else{
+      // CRIAR O SQL
+      $sql = "UPDATE cliente SET 
+            nome = '{$nome}',
+            telefone = '{$telefone}',
+            email = '{$email}',
+            cpf = '{$cpf}'
+            WHERE pk_cliente = {$id}";
+      // Tenta cadastrar, retorna true ou false
+      $resposta = $conn->query($sql);
+      // se true, verdadeiro, cadastro efetuado
+      if($resposta === true){
+        $msg = "Atualizado com sucesso!";
+      }else{
+        $msg = "Erro ao cadastrar!". $conn->error;
+      }
+    }
+  }
+?>
+
+<?php
+    include "includes/conexao.php";
     $id = $_GET['id'];
     $sql = "SELECT * FROM cliente WHERE pk_cliente ={$id}";
     $lista = $conn->query($sql);
     $cliente = $lista->fetch_assoc();
-
 ?>
 
-
-
-<?php include 'includes/header.php';?>
+<?php include "includes/header.php"; ?>
+    
 
 
 <div class="container">
+   <h1>Dados do Cliente</h1>
 
-        <h1>Dados do Cliente</h1>
+   <form class="form-horizontal" method="post" action="cliente-visualizar.php?id=<?php echo $cliente['pk_cliente']; ?>">
+  <fieldset>
 
-        <form class="form-horizontal" method="post" action="cliente-visualizar.php?id=<?php echo $cliente['pk_cliente'];?>"> 
+  <input value="<?php echo $cliente['pk_cliente']; ?>" id="id" name="id" type="hidden">
 
-        <fieldset>
-
-        <!-- Form Name -->
-        
-
-        <!-- Text input-->
-        <div class="form-group">
-        <label class="col-md-4 control-label" for="nome">Nome</label>  
-        <div class="col-md-4">
-        <input value ="<?php echo $cliente['nome'];?>" id="nome" name="nome" type="text" placeholder="" class="form-control input-md" required="">
-            
-        </div>
-        </div>
-
-        <!-- Text input-->
-        <div class="form-group">
-        <label class="col-md-4 control-label" for="telefone">Telefone</label>  
-        <div class="col-md-4">
-        <input value ="<?php echo $cliente['telefone'];?>"id="telefone" name="telefone" type="text" placeholder="" class="form-control input-md" required="">
-            
-        </div>
-        </div>
-
-        <!-- Text input-->
-        <div class="form-group">
-        <label class="col-md-4 control-label" for="email">E-mail</label>  
-        <div class="col-md-4">
-        <input value ="<?php echo $cliente['email'];?>"id="email" name="email" type="text" placeholder="" class="form-control input-md" required="">
-            
-        </div>
-        </div>
-
-        <!-- Text input-->
-        <div class="form-group">
-        <label class="col-md-4 control-label" for="cpf">CPF</label>  
-        <div class="col-md-4">
-        <input value ="<?php echo $cliente['cpf'];?>"id="cpf" name="cpf" type="text" placeholder="" class="form-control input-md" required="">
-            
-        </div>
-        </div>
-
-        <!-- Button -->
-        <div class="form-group">
-        <label class="col-md-4 control-label" for="cadastro"></label>
-        <div class="col-md-4">
-            <button id="cadastro" name="cadastro" class="btn btn-primary">Atualizar</button>
-        </div>
-        </div>
-
-        </fieldset>
-        </form>
-        <div> <?php if(isset($msg)) echo $msg; // se existir $msg imprima ?> </div>
-
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="id">ID</label>  
+  <div class="col-md-4">
+  <input value="<?php echo $cliente['pk_cliente']; ?>" id="id" name="id" disabled="disabled" type="text" placeholder="" class="form-control input-md">
+    
+  </div>
 </div>
 
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="nome">Nome</label>  
+  <div class="col-md-4">
+  <input value="<?php echo $cliente['nome']; ?>" id="nome" name="nome" type="text" placeholder="" class="form-control input-md">
+    
+  </div>
+</div>
 
-<?php include 'includes/footer.php'; ?>
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="telefone">Telefone</label>  
+  <div class="col-md-4">
+  <input value="<?php echo $cliente['telefone']; ?>" id="telefone" name="telefone" type="text" placeholder="" class="form-control input-md" required="">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="email">E-mail</label>  
+  <div class="col-md-4">
+  <input value="<?php echo $cliente['email']; ?>" id="email" name="email" type="text" placeholder="" class="form-control input-md" required="">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="cpf">CPF</label>  
+  <div class="col-md-4">
+  <input  value="<?php echo $cliente['cpf']; ?>" id="cpf" name="cpf" type="text" placeholder="" class="form-control input-md" required="">
+    
+  </div>
+</div>
+
+<!-- Button -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="singlebutton"></label>
+  <div class="col-md-4">
+    <button id="singlebutton" name="singlebutton" class="btn btn-primary">Atualizar</button>
+  </div>
+</div>
+
+</fieldset>
+</form>
+
+  <div>
+    <?php if(isset($msg)) echo $msg; // se existir $msg, imprima ?>
+  </div>
+
+
+    </div>
+    
+
+<?php include "includes/footer.php"; ?>
